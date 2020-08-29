dd7fc18e5837c0b3ee7f124c72f113ae26c04e8c
nicolas <nicolas@cookunity.com>
2020-08-29 17:26:58 -0300

    Init repo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '19b24d1bf1440158ef4fc662523c5962'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e282a165-07ba-435e-8bec-c958ce442033'
+};
